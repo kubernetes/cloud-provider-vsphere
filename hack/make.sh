3b526d7a23eba124ded47299a840724e17983208
akutz <akutz@vmware.com>
2019-03-10 15:33:39 -0500

    Parse Go version from .travis.yml for hack/make.sh
    This patch updates "hack/make.sh" to use the same version of Go defined
    in the ".travis.yml" file at the root of the project. This ensures
    calling "hack/make.sh" builds the controller using the same version of
    Go that builds the controller on Travis-CI.

--- hack/make.sh
+++ hack/make.sh
@@ -17,10 +17,25 @@
 # posix compliant
 # verified by https://www.shellcheck.net
 
-# Simple script to offer the ability to run the make directives in a Docker Container.
-# In other words you don't need a Go env on your system to run Make (build, test etc)
-# This script will just bind-mount the source directory into a container under the correct
-# GOPATH and handle all of the Go ENV stuff for you.  All you need is Docker
+# Runs make inside a Docker container.
+
+# Change directories to the project's root directory.
+# shellcheck disable=2128
+if [ -n "${BASH_SOURCE}" ]; then
+  # shellcheck disable=2039
+  HACK_DIR="$(dirname "${BASH_SOURCE[0]}")"
+elif command -v python >/dev/null 2>&1; then
+  HACK_DIR="$(python -c "import os; print(os.path.realpath('$(dirname "${0}")'))")"
+elif [ -d "../.git" ]; then
+  HACK_DIR="$(pwd)"
+elif [ -d ".git" ]; then
+  HACK_DIR="$(pwd)/hack"
+fi
+[ -n "${HACK_DIR}" ] || { echo "unable to find project root" 1>&2; exit 1; }
+cd "${HACK_DIR}/.." || { echo "unable to cd to project root" 1>&2; exit 1; }
+
+# Get the version of Go to use.
+GO_VERSION="$(grep '^go:' <".travis.yml" | sed 's/^\(go:[[:space:]]\{0,\}\"\{0,1\}\)\([^[:space:]\"]\{1,\}\)\(\"\{0,1\}\)$/\2/')"
 
 # When in an interactive terminal add the -t flag so Docker inherits
 # a pseudo TTY. Otherwords SIGINT does not work to kill the container
@@ -32,4 +47,4 @@ echo "${-}" | grep -q i && TERM_FLAGS="${TERM_FLAGS}t"
 docker run --rm ${TERM_FLAGS} ${DOCKER_OPTS} \
   -v "$(pwd)":/build:z \
   -w /build \
-  golang:1.11.5 make "${@}"
+  golang:"${GO_VERSION}" make "${@}"
