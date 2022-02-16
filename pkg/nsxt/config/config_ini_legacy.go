e1cfe6744b8da14317a5994b3a35a91648a95171
lubronzhan <lzhan@vmware.com>
2022-02-15 23:21:35 -0800

    Add missing logic to pass RemoteAuth when constructing Config

--- pkg/nsxt/config/config_ini_legacy.go
+++ pkg/nsxt/config/config_ini_legacy.go
@@ -36,6 +36,7 @@ func (nci *NsxtConfigINI) CreateConfig() *Config {
 	cfg.Password = nci.NSXT.Password
 	cfg.Host = nci.NSXT.Host
 	cfg.InsecureFlag = nci.NSXT.InsecureFlag
+	cfg.RemoteAuth = nci.NSXT.RemoteAuth
 	cfg.VMCAccessToken = nci.NSXT.VMCAccessToken
 	cfg.VMCAuthHost = nci.NSXT.VMCAuthHost
 	cfg.ClientAuthCertFile = nci.NSXT.ClientAuthCertFile
