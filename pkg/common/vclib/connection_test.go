dc5c03ddd8217576d5b605a67e37e1a88d29aca7
Nolan Brubaker <nolan@nbrubaker.com>
2023-05-10 14:10:24 -0400

    Fix error verification to work on Go 1.19 and 1.20
    Go 1.20 introduces a new tls.CertificateValidationError that wraps the
    x509.UnknownAuthorityError. Instead of casting to the
    UnknownAuthorityError directly, search the error chain for it.
    
    Signed-off-by: Nolan Brubaker <nolan@nbrubaker.com>

--- pkg/common/vclib/connection_test.go
+++ pkg/common/vclib/connection_test.go
@@ -30,6 +30,8 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/pkg/errors"
+
 	"k8s.io/cloud-provider-vsphere/pkg/common/vclib"
 	"k8s.io/cloud-provider-vsphere/pkg/common/vclib/fixtures"
 )
@@ -188,7 +190,8 @@ func verifyWrappedX509UnkownAuthorityErr(t *testing.T, err error) {
 	if !ok {
 		t.Fatalf("Expected to receive an url.Error, got '%s' (%#v)", err.Error(), err)
 	}
-	x509Err, ok := urlErr.Err.(x509.UnknownAuthorityError)
+	x509Err := &x509.UnknownAuthorityError{}
+	ok = errors.As(urlErr.Err, x509Err)
 	if !ok {
 		t.Fatalf("Expected to receive a wrapped x509.UnknownAuthorityError, got: '%s' (%#v)", urlErr.Error(), urlErr)
 	}
