6ce92a840e5bf513cadb59f577281956287968ff
Xudong Liu <xudong.liu@broadcom.com>
2024-03-07 12:12:00 +0800

    Update bump-k8s-dep.sh

--- hack/bump-k8s-dep.sh
+++ hack/bump-k8s-dep.sh
@@ -22,30 +22,35 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-
 dependencies=("k8s.io/api" "k8s.io/client-go" "k8s.io/cloud-provider" "k8s.io/apimachinery" "k8s.io/code-generator" "k8s.io/component-base" "k8s.io/klog/v2")
 
 check_and_bump_dependency() {
   dep=$1
   current_version=$(go list -m -f '{{.Version}}' "${dep}")
-  # latest_stable_version=$(go list -m -u -json ${dep} | jq -r .Version)
   latest_version=$(go list -m -versions -json "${dep}" | jq -r '.Versions[-1]')
+  # latest_stable_version=$(go list -m -u -json ${dep} | jq -r .Version)
+
+  # filter out the alpha release
+  if [[ $latest_version =~ alpha\.([0-9]+)$ ]]; then
+    echo "Skip auto bump for alpha release: [$dep@$current_version]"
+    return
+  fi
 
   echo "Current $dep version: $current_version"
   echo "Latest $dep version: $latest_version"
 
   # Bump the version if needed
- if [ "$current_version" == "$latest_version" ]; then
+  if [ "$current_version" == "$latest_version" ]; then
     echo "$dep@$current_version is already up to date."
   else
-    echo "Updating $dep to the $latest_version..."
+    echo "Updating $dep to $latest_version ..."
     go get -u "${dep}"@"${latest_version}"
   fi
 }
 
 # Loop through the list of dependencies
 for dep in "${dependencies[@]}"; do
-    check_and_bump_dependency "$dep"
+  check_and_bump_dependency "$dep"
 done
 
 go mod tidy
