4fc36e603bb420bb666d8ccdd92bfff2ac1b141b
Ricardo Pchevuzinske Katz <ricardo.katz@broadcom.com>
2025-06-05 17:07:44 -0300

    Remove dead code or unused code from CPI

--- pkg/common/vclib/folder.go
+++ pkg/common/vclib/folder.go
@@ -17,31 +17,11 @@ limitations under the License.
 package vclib
 
 import (
-	"context"
-
 	"github.com/vmware/govmomi/object"
-	klog "k8s.io/klog/v2"
 )
 
 // Folder extends the govmomi Folder object
 type Folder struct {
 	*object.Folder
 	Datacenter *Datacenter
-}
-
-// GetVirtualMachines returns list of VirtualMachine inside a folder.
-func (folder *Folder) GetVirtualMachines(ctx context.Context) ([]*VirtualMachine, error) {
-	vmFolders, err := folder.Children(ctx)
-	if err != nil {
-		klog.Errorf("Failed to get children from Folder: %s. err: %+v", folder.InventoryPath, err)
-		return nil, err
-	}
-	var vmObjList []*VirtualMachine
-	for _, vmFolder := range vmFolders {
-		if vmFolder.Reference().Type == VirtualMachineType {
-			vmObj := VirtualMachine{object.NewVirtualMachine(folder.Client(), vmFolder.Reference()), folder.Datacenter}
-			vmObjList = append(vmObjList, &vmObj)
-		}
-	}
-	return vmObjList, nil
 }
